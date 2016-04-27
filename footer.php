71e562ba94f06566c3bfed3dd8604029cfc52bc3
erikbelusic <erikbelusic@outlook.com>
2016-04-26 21:55:22 -0400

    add wp_footer call and restructure markup

--- footer.php
+++ footer.php
@@ -1,7 +1,21 @@
+</div><!-- .col-xs-12 -->
+</div><!-- .container -->
+</div><!-- .row -->
+<footer class="site-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 text-center">
+                <span class="copyright">&copy; <?= date('Y'); ?> <?php bloginfo(); ?> All Rights Reserved.</span>
+                <span class="poweredby">Code by EB</span>
+            </div>
+        </div>
+    </div><!-- .container -->
+</footer>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+<?php wp_footer(); ?>
 </body>
 </html>
