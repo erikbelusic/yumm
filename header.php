b4a4e8037581c227e7519101b52e79714adcf9ad
erikbelusic <erikbelusic@outlook.com>
2016-04-28 19:45:47 -0400

    move all assets to be enqued via wordpress functions

--- header.php
+++ header.php
@@ -1,20 +1,11 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="utf-8">
+    <meta charset="<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title><?php bloginfo(); ?><?php wp_title('|'); ?></title>
-
-    <!-- Bootstrap -->
-    <!-- Latest compiled and minified CSS -->
-    <link href='https://fonts.googleapis.com/css?family=Bitter:400,700|Open+Sans:300,700' rel='stylesheet'
-          type='text/css'>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
-          integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/dist/css/main.css">
-
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
---
+++ lib/scripts.php
@@ -0,0 +1,15 @@
+<?php
+if (!function_exists('yumm_theme_assets')) {
+    /**
+     * Theme assets
+     */
+    function yumm_theme_assets()
+    {
+        wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Bitter:400,700|Open+Sans:300,700', false, null);
+        wp_enqueue_style('yumm-css', get_template_directory_uri() . '/dist/css/main.css', false, null);
+
+        wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js', [], null, true);
+        wp_enqueue_script('yumm-js', get_template_directory_uri() . '/assets/js/main.js', ['jquery'], null, true);
+    }
+}
+add_action('wp_enqueue_scripts', 'yumm_theme_assets');
