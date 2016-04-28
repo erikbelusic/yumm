c756b52391fbc272c515e80e65adda2d863e89d7
erikbelusic <erikbelusic@outlook.com>
2016-04-28 19:37:13 -0400

    make posts expandable

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,25 @@
 (function ($) {
-    $('.post .post-excerpt').click(function () {
-        var $parentEl = $(this).parents('.home-item');
-        $parentEl.addClass('expanded');
-    });
+    if ($('body').hasClass('home')) {
+        $('.post .post-excerpt').each(function(){
+            $(this).css({height:$(this).height()});
+        });
+        $('.post .post-excerpt, .close-button').click(function () {
+            var $parentEl = $(this).parents('.home-item');
+            var $excerpt = $(this).hasClass('post-excerpt') ? $(this) : $(this).closest('.post').find('.post-excerpt');
+            var $content = $(this).siblings('.post-full-content');
+            $parentEl.toggleClass('expanded');
+            if ($parentEl.hasClass('expanded')) {
+                // time to open...
+                $excerpt.css({height: 0, opacity: 0});
+                setTimeout(function(){
+                    $content.delay(500).css({height: $content.children('.content-measuring-wrapper').height(), opacity: 1});
+                },500);
+            } else {
+                $content.css({height: 0, opacity: 0});
+                setTimeout(function(){
+                    $excerpt.css({height: $excerpt.children('.excerpt-measuring-wrapper').height(), opacity: 1});
+                },1000);
+            }
+        });
+    }
 })(jQuery);
--- templates/content.php
+++ templates/content.php
@@ -2,6 +2,14 @@
     <div class="close-button pull-right"><i class="glyphicon glyphicon-remove"></i></div>
     <a href="<?php the_permalink(); ?>"><h2 class="post-title"><?php the_title(); ?></h2></a>
     <span class="post-date"><span class="post-date"><?php get_template_part('templates/meta'); ?></span></span>
-    <div class="post-excerpt"><?php the_excerpt(); ?></div>
-    <div class="post-full-content"><?php the_content(); ?></div>
+    <div class="post-excerpt">
+        <div class="excerpt-measuring-wrapper">
+            <?php the_excerpt(); ?>
+        </div>
+    </div>
+    <div class="post-full-content">
+        <div class="content-measuring-wrapper">
+            <?php the_content(); ?>
+        </div>
+    </div>
 </article>
