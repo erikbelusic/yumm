9c0f6c3ce21c894133fe010b995db1050e8a3f75
erikbelusic <erikbelusic@outlook.com>
2016-04-27 20:43:12 -0400

    fix mobile layout so image appears first

--- templates/content-single-recipe.php
+++ templates/content-single-recipe.php
@@ -1,7 +1,11 @@
 <article <?php post_class(); ?>>
-
         <div class="row">
-            <div class="col-md-6">
+            <div class="col-md-6 col-md-push-6">
+                <?php if (has_post_thumbnail()) : ?>
+                    <?php the_post_thumbnail('large', 'class=img-responsive'); ?>
+                <?php endif; ?>
+            </div>
+            <div class="col-md-6 col-md-pull-6">
                 <header>
                 <h1 class="post-title"><?php the_title(); ?></h1>
                 <h4 class="post-author">Recipe by: <?php the_author(); ?></h4>
@@ -13,10 +17,6 @@
                     <?php the_content(); ?>
                 </section>
             </div>
-            <div class="col-md-6">
-                <?php if (has_post_thumbnail()) : ?>
-                    <?php the_post_thumbnail('large', 'class=img-responsive'); ?>
-                <?php endif; ?>
-            </div>
+
         </div>
 </article>
