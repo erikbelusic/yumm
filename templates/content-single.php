5a1ddcb5d6874d3a690c500f899d5384c2840c6c
erikbelusic <erikbelusic@outlook.com>
2016-04-27 20:42:59 -0400

    extract published date into a partial

--- templates/content-single.php
+++ templates/content-single.php
@@ -2,7 +2,7 @@
     <div class="row">
         <div class="col-sm-8 col-sm-offset-2">
             <h1 class="post-title"><?php the_title(); ?></h1>
-            <span class="post-date"><?= get_the_date(); ?></span>
+            <span class="post-date"><?php get_template_part('templates/meta'); ?></span>
             <div class="post-content"><?php the_content(); ?></div>
         </div>
     </div>
---
+++ templates/meta.php
@@ -0,0 +1,1 @@
+Posted on: <time datetime="<?= get_the_date('Y-m-d H:i'); ?>"><?= get_the_date(); ?></time>
