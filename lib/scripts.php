4a40379260e9146e8148e58aeb3fe721579a17c0
erikbelusic <erikbelusic@outlook.com>
2016-04-28 21:30:13 -0400

    copy fonts into dist folder and remove cdn fonts from old bootstrap

--- lib/scripts.php
+++ lib/scripts.php
@@ -6,7 +6,6 @@ if (!function_exists('yumm_theme_assets')) {
     function yumm_theme_assets()
     {
         wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Bitter:400,700%7COpen+Sans:300,700', false, null);
-        wp_enqueue_style('glyphicons', '//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-glyphicons.css', false, null);
         wp_enqueue_style('yumm-css', get_template_directory_uri() . '/dist/css/main.css', false, null);
 
         wp_enqueue_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js', [], null, true);
