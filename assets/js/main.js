0cd90ad6e9ca14ca3a634c5120020f136dc891e4
erikbelusic <erikbelusic@outlook.com>
2016-04-28 20:11:13 -0400

    add basic comments to js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,36 @@
 (function ($) {
+
+    /*
+     * Only run on home page.
+     */
     if ($('body').hasClass('home')) {
+
+        /*
+         * Set the height of all post excerpts explicitly from its actual height with rendered content.
+         */
         $('.post .post-excerpt').each(function(){
             $(this).css({height:$(this).height()});
         });
+
+        /*
+         * Handle post expand/contract toggle
+         */
         $('.post .post-excerpt, .close-button').click(function () {
+
             var $parentEl = $(this).parents('.home-item');
-            var $excerpt = $(this).hasClass('post-excerpt') ? $(this) : $(this).closest('.post').find('.post-excerpt');
+            var $excerpt = $(this).hasClass('post-excerpt') ? $(this) : $(this).closest('.post').find('.post-excerpt'); // was the excerpt clicked or the close button?
             var $content = $(this).siblings('.post-full-content');
+
             $parentEl.toggleClass('expanded');
+
             if ($parentEl.hasClass('expanded')) {
-                // time to open...
                 $excerpt.css({height: 0, opacity: 0});
-                setTimeout(function(){
+                setTimeout(function(){ // delay the second animation
                     $content.delay(500).css({height: $content.children('.content-measuring-wrapper').height(), opacity: 1});
                 },500);
             } else {
                 $content.css({height: 0, opacity: 0});
-                setTimeout(function(){
+                setTimeout(function(){ // delay the second animation
                     $excerpt.css({height: $excerpt.children('.excerpt-measuring-wrapper').height(), opacity: 1});
                 },1000);
             }
